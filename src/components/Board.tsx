6438df775dbc2a49a31ec3f29206075027e1e491
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-01-15 17:39:16 +0900

    determine winner
    승패 결정하기

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,12 +2,29 @@ import { useState } from "react";
 import "../css/Board.css";
 import Square from "./Square";
 
+const calucateWinner = (squares: (string|null)[]): string|null => {
+  const lines = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,5],[2,4,6]
+  ];
+  for(const line of lines) {
+    const [a,b,c] = line;
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
 const Board = () => {
   const [ squares, setSquares ] = useState(Array(9).fill(null));
+  const [ isNext, setNext ] = useState(false);
 
   const handleClick = (i: number) => {
     const newSquares = squares.slice();
-    newSquares[i] = 'X';
+    newSquares[i] = isNext ? 'O' : 'X';
+    setNext(isNext => !isNext);
     setSquares(newSquares);
   };
 
@@ -15,7 +32,8 @@ const Board = () => {
     return (<Square value={ squares[i] } onClick={() => handleClick(i)}/>);
   };
 
-  const status = 'Next Plyaer: X';
+  const winner = calucateWinner(squares);
+  const status = winner ? `winner : ${winner}` : squares.every((square) => square !== null ) ? 'Draw!' : `Next Plyaer: ${isNext ? 'O' : 'X'}`;
 
   return (
     <div>
